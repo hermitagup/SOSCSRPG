d03d8388ac636def955882f7dc70e18e4c186d12
KWeisz <kbabiarz93+github@gmail.com>
2020-03-30 12:02:01 -0700

    Wykonano lekcje 03.4

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models 
+{
+    public class Player // klasa player, z parametrami
+    {
+        public string Name { get; set; }
+        public string CharacterClass { get; set; }
+        public int HitPoints { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+
+
+    }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; } // ustawienie klasy 'Player' 
+        
+        public GameSession() // Konstruktor (funkcja) gamesession
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Scott";
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
