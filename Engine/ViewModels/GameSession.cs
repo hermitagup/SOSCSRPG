e30bc7f9f64090cf3960472b342cdb00b8320cac
BarkUp <hermitagup@users.noreply.github.com>
2020-03-30 20:57:35 +0200

    CodeSession 4 - up to 03.4

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()    // GameSession constructor - part of a code run when object is being created
+        {
+            CurrentPlayer = new Player();   // Ewaluate what is on the right side of '=' and put on left side (property CurrentPlayer)
+            CurrentPlayer.Name = "Scott";
+            CurrentPlayer.Gold = 100000;
+        }
+    }
+}
