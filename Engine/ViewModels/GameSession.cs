ba2f21ebbd082a3a5fb353ac24f018d9f49295a6
BarkUp <hermitagup@users.noreply.github.com>
2020-04-03 14:53:40 +0200

    End of lesson  03.5

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -7,15 +7,20 @@ using Engine.Models;
 
 namespace Engine.ViewModels
 {
-    class GameSession
+    public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
 
         public GameSession()    // GameSession constructor - part of a code run when object is being created
         {
-            CurrentPlayer = new Player();   // Ewaluate what is on the right side of '=' and put on left side (property CurrentPlayer)
+            CurrentPlayer = new Player();   //  Instantiating Player object|| Evaluate what is on the right side of '=' and put on left side (property CurrentPlayer)
+            // Below data are temp and for testing time only (This will display This data from This View Model via XAML Main Window
             CurrentPlayer.Name = "Scott";
-            CurrentPlayer.Gold = 100000;
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
         }
     }
 }
