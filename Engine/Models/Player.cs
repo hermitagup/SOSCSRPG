f48b74d3f2e6c167ec11e4e6565c2d9f043c415a
BarkUp <hermitagup@users.noreply.github.com>
2020-04-06 11:45:52 +0200

    End of lesson 03.6

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,18 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    public class Player // klasa player, z parametrami
+    public class Player : INotifyPropertyChanged // klasa player, z parametrami { INotifyPropertyChange will notify that change to any of the property in a Player class was made and anything that is using those properties will know to update values accordingly
     {
-        public string Name { get; set; }
-        public string CharacterClass { get; set; }
-        public int HitPoints { get; set; }
-        public int ExperiencePoints { get; set; }
-        public int Level { get; set; }
-        public int Gold { get; set; }
+        private string _name;
+        private string _characterClass;
+        private int _hitPoints;
+        private int _experiencePoints;
+        private int _level;
+        private int _gold;
+
+        public string Name {
+            get { return _name; }
+            set { 
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string CharacterClass {
+            get { return _characterClass; }
+            set {
+                _characterClass = value;
+                OnPropertyChanged("CharacterClass");
+            }
+        }
+        public int HitPoints {
+            get { return _hitPoints; }
+            set {
+                _hitPoints = value;
+                OnPropertyChanged("HitPoints");
+            }
+        }
+        public int ExperiencePoints
+        {
+            get { return _experiencePoints; }
+            set 
+            { 
+                _experiencePoints = value;
+                OnPropertyChanged("ExperiencePoints");  // This is exact property name that will be used in below OnPropertyChanged
+            }
+        }
+        public int Level {
+            get { return _level; }
+            set {
+                _level = value;
+                OnPropertyChanged("Level");
+            }
+        }
+        public int Gold {
+            get { return _gold; }
+            set {
+                _gold = value;
+                OnPropertyChanged("Gold");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName){
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -21,12 +21,17 @@ namespace WPFUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private GameSession _GameSession;  //Declaring private variable
+        private GameSession _gameSession;  //Declaring private variable
         public MainWindow()
         {
             InitializeComponent();
-            _GameSession = new GameSession(); // Instantiating new GameSession object when starting new window| Now our View will have player and game session to work with (Player object is instantiatet in GameSession class.
-            DataContext = _GameSession;  // This is what is XAML file is going to use for it's values.
+            _gameSession = new GameSession(); // Instantiating new GameSession object when starting new window| Now our View will have player and game session to work with (Player object is instantiatet in GameSession class.
+            DataContext = _gameSession;  // This is what is XAML file is going to use for it's values.
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;  // in short _gameSession.CurrentPlayer.ExperiencePoints +=10;
         }
     }
 }
