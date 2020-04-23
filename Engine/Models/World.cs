ca79dff3299a72141704bfded3cbda56ace69b97
KWeisz <kbabiarz93+github@gmail.com>
2020-04-23 10:51:37 -0700

    Completed 04.2

---
+++ Engine/Models/World.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class World
+    {
+        private List<Location> _locations = new List<Location>(); //create locations variable, private, only accessed inside wordl object
+    
+        internal void AddLocation(int xCoordinate, int yCoordinate, string name, string description, string imageName) //only worldfactory uses this calss, when we call it we don't want any answer back (function doesn't return any value)
+        {
+            Location loc = new Location(); //properties are the parameter which we receive
+            loc.XCoordinate = xCoordinate;
+            loc.YCoordinate = yCoordinate;
+            loc.Name = name;
+            loc.Description = description;
+            loc.ImageName = imageName;
+
+            _locations.Add(loc); //put new location object in locations
+     
+        }
+
+        public Location LocationAt (int xCoordinate, int yCoordinate) //public, because it will be called from other objects
+        {
+            foreach(Location loc in _locations) //look for each object in _locations, if loc has coordinates then return that location
+            {
+                if(loc.XCoordinate == xCoordinate && loc.YCoordinate == yCoordinate)
+                {
+                    return loc;
+                }
+            }
+
+            return null; //checked every location, if none of them matches, return null
+        }
+    }
+}
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Engine.Models;
+using Engine.Factories;
 
 namespace Engine.ViewModels
 {
     public class GameSession
     {
+        public World CurrentWorld { get; set; }
+
         public Player CurrentPlayer { get; set; }
 
         public Location CurrentLocation { get; set; }
@@ -24,12 +27,10 @@ namespace Engine.ViewModels
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
 
-            CurrentLocation = new Location();
-            CurrentLocation.Name = "Home";
-            CurrentLocation.XCoordinate = 0;
-            CurrentLocation.YCoordinate = -1;
-            CurrentLocation.Description = "This is your house";
-            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+
+            CurrentLocation = CurrentWorld.LocationAt(0, 0); //
         }
     }
 }
