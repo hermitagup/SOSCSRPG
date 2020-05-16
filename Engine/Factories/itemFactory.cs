0c7360651ec14861c07f6ad2fa1dca7925626d56
BarkUp <hermitagup@users.noreply.github.com>
2020-05-16 18:28:05 +0200

    Lesson 05.1 completed

---
+++ Engine/Factories/itemFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    public static class itemFactory                             // it's static as we will not need to instanciated it but just use functions from it 
+    {                                                           // static class never have constructor as it is never create an instance (not being constructed)
+        private static List<GameItem> _standardGameItems;
+        static itemFactory() {                                   // this function is being used with first run (something like constructor but for static functions.
+            _standardGameItems = new List<GameItem>();           // this will create an empty list as we can add items only to empty list , can't add to null
+
+            _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
+            _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
+        }
+        public static GameItem CreateGameItem(int itemTypeID)
+        {
+            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);       // this will find a first item on '_standardGameItems' list, 
+                                                                                                                    // that has a 'ItemTypeID' property value equal to the one we passed to a function
+                                                                                                                    // if match not found it will use default item (here null)
+                                                                                                                    // conclusion: Our 'standardItem' will have matching item from our list or null
+            if (standardItem != null)
+            {
+                return standardItem.Clone();
+            }
+            return null;    //if standardItem not found on a list, return null;
+        }
+    }
+}
---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Weapon : GameItem  // The Weapon class is a sub class of GameItem class
+    {
+        public int MinimumDamage { get; set; }
+        public int MaximumDamage { get; set; }
+        public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage)  // this will take parameters 'itemTypeID', 'name', 'price' and send their values to base class which is a 'GameItem' class in here
+            : base(itemTypeID, name, price){
+            MinimumDamage = minDamage;
+            MaximumDamage = maxDamage;
+        }
+        public new Weapon Clone()  // as Weapon class is a sub class of GameItem and both have same function 'Clone' we need to confirm that we want to use class from Weapon not from GameItem by using 'new' word.
+        {   
+            return new Weapon(ItemTypeID, Name, Price, MinimumDamage, MaximumDamage);
+        }
+    }
+}
