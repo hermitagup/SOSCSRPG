1dba9b6f6a758cd30074dc7e58f816726a1707ed
KWeisz <pc-user@DESKTOP-4L534T8>
2020-05-19 18:09:12 +0200

    Completed 05.1

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    public static class ItemFactory
+    {
+        private static List<GameItem> _standardGameItems;
+
+        static ItemFactory()
+        {
+            _standardGameItems = new List<GameItem>(); // create new list gameitems (empty) and assign that list to a variable
+
+            _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
+            _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
+        }
+
+        public static GameItem CreateGameItem(int itemTypeID)
+        {
+            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID); // on a list variable it uses linq to find 1 item that has properites
+
+            if (standardItem != null)
+            {
+                return standardItem.Clone(); //take standarditem, make copy of it and return that from CreateGameItem function
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    class Weapon : GameItem // the weapon class is a subclass of GameItem
+    {
+        public int MinimumDamage { get; set; }
+        public int MaximumDamage { get; set; }
+
+        public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage) // consturctor to send items to base class via Weapon class?
+            : base(itemTypeID, name, price)
+        {
+            MinimumDamage = minDamage;
+            MaximumDamage = maxDamage;
+        }
+
+        public new Weapon Clone ()
+        {
+            return new Weapon(ItemTypeID, Name, Price, MinimumDamage, MaximumDamage); //instuatnce new weapon
+        }
+    }
+}
