1ac2e6046e6cedc5b818a1e84bd21b36d432bba2
KWeisz <KWeisz@Kweisz.com>
2021-04-14 18:58:13 +0200

    Lesson 11.1

---
+++ TestEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestEngine")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("932baa74-80f1-403f-896d-a94b1ab7b955")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestEngine/ViewModels/TestGameSession.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Engine.ViewModels; // to access the GameSession class in the Engine.ViewModels namespace
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestEngine.ViewModels {
+
+    [TestClass] // unit testcalss
+    public class TestGameSession {
+
+        [TestMethod] // this function is a unit test function
+        public void TestCreateGameSession() {
+
+            GameSession gameSession = new GameSession();
+
+            Assert.IsNotNull(gameSession.CurrentPlayer);
+            Assert.AreEqual("Town square", gameSession.CurrentLocation.Name);
+        }
+
+        [TestMethod]
+        public void TestPlayerMovesHomeAndIsCompletelyHealedOnKilled() {
+
+            GameSession gameSession = new GameSession();
+
+            gameSession.CurrentPlayer.TakeDamage(999);
+
+            Assert.AreEqual("Home", gameSession.CurrentLocation.Name);
+            Assert.AreEqual(gameSession.CurrentPlayer.Level * 10, gameSession.CurrentPlayer.CurrentHitPoints);
+        }
+    }
+}
