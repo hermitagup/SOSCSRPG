9abee3c19aad38180b9528626f4fc5d50aa23e6b
ofca127 <ofca@lokalny127.eu>
2021-12-15 18:24:28 +0100

    Completed Lesson 12.6

--- Engine/Actions/AttackWithWeapon.cs
+++ Engine/Actions/AttackWithWeapon.cs
@@ -3,17 +3,16 @@ using Engine.Models;
 
 namespace Engine.Actions
 {
-    public class AttackWithWeapon : IAction {   // Interface implementation of IAction interface | this allows to treay "AttackWithWeapon" obj as AttackWithWeapon obj or as IAction object
+    public class AttackWithWeapon : BaseAction,  IAction {   // Interface implementation of IAction interface | this allows to treay "AttackWithWeapon" obj as AttackWithWeapon obj or as IAction object
                                                 // when treat as IAction obj  - the only thing available are the one listed within interface IAction file.
-        private readonly GameItem _weapon;
         private readonly int _maximumDamage;
         private readonly int _minimumDamage;
 
-        public event EventHandler<string> OnActionPerformed;    // public event that will notify UI of any messages that result from executing this command object
-
-        public AttackWithWeapon(GameItem weapon, int minimumDamage, int maximumDamage){  // constructor with parameter validation and to private variables pass
-            if (weapon.Category != GameItem.ItemCategory.Weapon) {
-                throw new ArgumentException($"{weapon.Name} is not a weapon");
+        public AttackWithWeapon(GameItem itemInUse, int minimumDamage, int maximumDamage)
+            : base(itemInUse)
+        {  // constructor with parameter validation and to private variables pass
+            if (itemInUse.Category != GameItem.ItemCategory.Weapon) {
+                throw new ArgumentException($"{itemInUse.Name} is not a weapon");
             }
 
             if (_minimumDamage < 0) {
@@ -23,8 +22,6 @@ namespace Engine.Actions
             if (_maximumDamage < _minimumDamage) {
                 throw new ArgumentException("maximumDamage must be >= minimumDamage");
             }
-
-            _weapon = weapon;
             _minimumDamage = minimumDamage;
             _maximumDamage = maximumDamage;
         }
@@ -42,11 +39,6 @@ namespace Engine.Actions
                 ReportResult($"{actorName} hit {targetName} for {damage} point{(damage > 1 ? "s" : "")}.");     // singular plural "s" at the end trick
                 target.TakeDamage(damage);
             }
-        }
-
-        // Function that raises the event notification - if anything subscribed to the OnActionPerformed event
-        private void ReportResult(string result) {
-            OnActionPerformed?.Invoke(this, result);
         }
     }
 }
